f409234f92436e09293ed97f8a7647af1d2f2f6d
Mal <czasacz@gmail.com>
2021-02-16 10:08:43 +0100

    Added Loader to  product fetch request.

--- react-learning-qw/src/Views/Product.js
+++ react-learning-qw/src/Views/Product.js
@@ -1,22 +1,53 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams} from "react-router-dom";
+import Loader from "../Components/Loader";
 
 function Product(){
 
     const { id } = useParams();
     const url = `https://602a44aa6c995100176ee169.mockapi.io/products/${id}`;
-    const [product,setProduct] = useState(null);
+    const [product,setProduct] = useState({
+        loading: false,
+        data: null,
+        error: false
+    });
 
     let content = null;
 
     useEffect(() => {
+        setProduct({
+            loading: true,
+            data: null,
+            error: false
+        });
         axios.get(url).then(response =>{
-            setProduct(response.data)
+            setProduct({
+                loading: false,
+                data: response.data,
+                error: false
+            });
+        }).catch(() =>{
+            setProduct({
+                loading: false,
+                data: null,
+                error: true
+            });
         });    
     },[url]);
 
-    if(product){
+    if(product.error){
+        content = 
+        <p>
+            There was an error please refresh or try again later
+        </p>
+    }
+
+    if(product.loading){
+        content = <Loader />
+    }
+
+    if(product.data){
         content =
         <div>
             <h1 className="text-2xl font-bold mb-3">
@@ -24,15 +55,15 @@ function Product(){
             </h1>
             <div>
                 <img 
-                    src={product.images[0].image}
-                    alt={product.name}
+                    src={product.data.images[0].image}
+                    alt={product.data.name}
                 />
             </div>
             <div className="font-bold text-xl mb-3">
-                $ {product.price}
+                $ {product.data.price}
             </div>
             <div>
-                {product.description}
+                {product.data.description}
             </div>
         </div>
     }
