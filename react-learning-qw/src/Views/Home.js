f6202a3b07d6d3c1bbb675194582ed07242406e5
Mal <czasacz@gmail.com>
2021-02-17 10:40:40 +0100

    Created HttpRequest

--- react-learning-qw/src/Views/Home.js
+++ react-learning-qw/src/Views/Home.js
@@ -2,39 +2,17 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import Loader from "../Components/Loader";
 import ProductCard from "../Components/ProductCard";
+import { useAxiosGet } from "../Hooks/HttpRequests";
 
 
 function Home(){
 
     const url = `https://602a44aa6c995100176ee169.mockapi.io/products?page=1&limit=10`;
-    const [products,setProducts] = useState({
-        loading: false,
-        data: null,
-        error: false
-    });
 
+    let products = useAxiosGet(url);
     let content = null;
 
-    useEffect(() => {
-        setProducts({
-            loading: true,
-            data: null,
-            error: false
-        });
-        axios.get(url).then(response =>{
-            setProducts({
-                loading: false,
-                data: response.data,
-                error: false
-            });
-        }).catch(() =>{
-            setProducts({
-                loading: false,
-                data: null,
-                error: true
-            });
-        });    
-    },[url]);
+    
 
     if(products.error){
         content = 
--- react-learning-qw/src/Views/Product.js
+++ react-learning-qw/src/Views/Product.js
@@ -2,39 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams} from "react-router-dom";
 import Loader from "../Components/Loader";
+import {useAxiosGet} from "../Hooks/HttpRequests";
 
 function Product(){
 
     const { id } = useParams();
     const url = `https://602a44aa6c995100176ee169.mockapi.io/products/${id}`;
-    const [product,setProduct] = useState({
-        loading: false,
-        data: null,
-        error: false
-    });
 
+    let product = useAxiosGet(url);
     let content = null;
-
-    useEffect(() => {
-        setProduct({
-            loading: true,
-            data: null,
-            error: false
-        });
-        axios.get(url).then(response =>{
-            setProduct({
-                loading: false,
-                data: response.data,
-                error: false
-            });
-        }).catch(() =>{
-            setProduct({
-                loading: false,
-                data: null,
-                error: true
-            });
-        });    
-    },[url]);
 
     if(product.error){
         content = 
